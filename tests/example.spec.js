ae10c2cb52119bc8921472acca688c6a55728aaf
emilicastells <e.castells@inpsyde.com>
2023-01-27 16:25:36 +0100

    Add ngrok

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,12 @@
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page, baseURL }) => {
+test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/DDEV Playwright Tests/);
+});
+
+test('has ngrok url', async ({page, baseURL}) => {
+  await page.goto('/');
+  console.log(baseURL)
   await expect(page).toHaveURL(baseURL);
 });
