01f53f0504b19be0ec1d8af542dff71aedbdd1e8
Emili Castells Guasch <ecastells@MacBook-Air-de-Emili.local>
2023-02-24 14:36:51 +0100

    Use base url from config

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test('has title', async ({ page, baseURL }) => {
+  await page.goto(baseURL);
   await expect(page).toHaveTitle(/DDEV Playwright Tests/);
 });
